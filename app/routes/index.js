6e47eeb642884ec3f7de484d79a2cd05861decb0
lhi <hilee.em@gmail.com>
2017-08-02 09:16:19 +0900

    # LHI incident 추가

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var log = require('log4js').getLogger("index");
 var ssc = require('../util/session');
-require('../util/cookie');
+//require('../util/cookie');
 
 const controller = require('../controllers/login');
 
@@ -10,9 +10,7 @@ const controller = require('../controllers/login');
 router.get('/', ssc.sessionCheck);
 
 router.route('/index').post(controller.logincheck)
-                           .get(ssc.sessionCheck, controller.retry);                           
+                      .get(ssc.sessionCheck, controller.retry);                           
 router.route('/logout').get(controller.logout);
-
-
 
 module.exports = router;
