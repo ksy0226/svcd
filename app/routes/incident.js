610812ec2af8aa3da0cda572c2c6588f40953af1
lhi <hilee.em@gmail.com>
2017-08-04 11:46:07 +0900

    # LHI default layout 수정

--- app/routes/incident.js
+++ app/routes/incident.js
@@ -2,30 +2,33 @@
 
 const express = require('express');
 const router = express.Router();
-
+const ssc = require('../util/session');
+const logger = require('log4js').getLogger('app');
+const multer = require('multer');
 
 const controller = require('../controllers/incident');
 
-/*
-router.get('/', controller.index);
-router.post('/', controller.save);
-router.get('/new', controller.new);
-router.get('/show/:id', controller.show);
-router.get('/edit/:id', controller.edit);
-router.post('/edit/:id', controller.update);
-router.get('/delete/:id', controller.delete);
-*/
+var path = require('path');
+var uploadDir = path.join( __dirname , '/upload-file' );
+var storage = multer.diskStorage({
+    destination : function (req, file, callback) {
+        callback(null, '/tmp/uploads');
+    },
+    filename : function (req, file, callback) {
+        callback(null, 'posts-' + Date.now() + '.'+ file.mimetype.split('/')[1] );
+    }
+});
+var upload = multer({ storage: storage });
+     
 
-router.route('/').get(controller.index);
+ router.route('/').get(ssc.sessionCheck, controller.index);
 //                 .post(controller.index);
 //router.route('/:searchType/:searchText').get(controller.index);
-router.route('/new').get(controller.new)
-                 .post(controller.save);
-               
-router.route('/show/:id').get(controller.show);
-router.route('/edit/:id').get(controller.edit)
-                         .post(controller.update);
-router.route('/delete/:id').get(controller.delete);
-
+router.get('/new', ssc.sessionCheck, controller.new)
+router.post('/new', upload.single('thumbnail') , ssc.sessionCheck, controller.save);             
+router.get('/show/:id', ssc.sessionCheck, controller.show);
+router.get('/edit/:id', ssc.sessionCheck, controller.edit)
+router.put('/edit/:id', ssc.sessionCheck, controller.update);
+router.delete('/delete/:id', ssc.sessionCheck, controller.delete);
 
 module.exports = router;
