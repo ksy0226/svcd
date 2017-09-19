43d8e126cf06a7088be6a059205d4d568dfeb1bb
sunwoo <psw@isu.co.kr>
2017-09-19 13:19:38 +0900

    #psw commit

--- app/models/CompanyProcess.js
+++ app/models/CompanyProcess.js
@@ -6,7 +6,7 @@ var CompanyProcessSchema = mongoose.Schema({
     company_cd: { type: String, required: true },     //회사
     higher_cd : {type: String, required:true},        //상위코드
     higher_nm : {type: String},        //상위코드
-    createdAt: { type: Date, default: Date.now },    //생성일자 
+    created_at: { type: Date, default: Date.now },    //생성일자 
     user_flag: { type: String }                      //사용여부(1:사용, 0:미사용)
 });
 
