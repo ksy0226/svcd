d9dbba6a4bae6ac4e4840e926a09bea17233d528
sykim <ksy0226@isu.co.kr>
2017-09-20 13:18:54 +0900

    #KSY usermanage/index.js

---
+++ app/public/isu/js/usermanage/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(document).ready(function() { 
+    $('#user_flag').val('#{usermanage.user_flag}');
+    $('#group_flag').val('#{usermanage.group_flag}');
+});
+
+function companyCd(){
+    //alert(JSON.stringify(companyObj));
+    //선택된 회사 인덱스 값
+    var sIdx = $('#company_cd option').index($('#company_cd option:selected'));
+    sIdx = sIdx -1; //'선택하세요' 인덱스값 1을 빼줌
+    //선택값 매핑
+    $('#company_nm').val($('#company_cd option:selected').text());
+    $('input[name="usermanage[dom_post_cd1]"]').val(companyObj[sIdx].zip_cd);
+    $('input[name="usermanage[dom_addr]"]').val(companyObj[sIdx].addr);
+    $('input[name="usermanage[dom_addr2]"]').val(companyObj[sIdx].addr2);
+    $('input[name="usermanage[office_tel_no]"]').val(companyObj[sIdx].tel_no);
+}
