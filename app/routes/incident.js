45b4917824ce71342d8ab9a255e14283e0fff0f9
sykim <ksy0226@isu.co.kr>
2017-09-27 11:02:29 +0900

    # KSY

--- app/routes/incident.js
+++ app/routes/incident.js
@@ -9,17 +9,9 @@ const upload = require('../util/multer');
 const controller = require('../controllers/incident');
 
 router.route('/').get(ssc.sessionCheck, controller.index);
-//                 .post(controller.index);
-router.route('/:searchType/:searchText').get(controller.index);
-router.get('/new/:title', ssc.sessionCheck, controller.new)
-router.post('/new', upload.array('incident[attach-file]'), ssc.sessionCheck, controller.save);
-//router.get('/show/:id', ssc.sessionCheck, controller.show);
-router.get('/show', ssc.sessionCheck, controller.show);
-router.get('/edit/:id', ssc.sessionCheck, controller.edit)
-router.put('/edit/:id', ssc.sessionCheck, controller.update);
-router.delete('/delete/:id', ssc.sessionCheck, controller.delete);
-
-router.get('/viewDetail/:id', ssc.sessionCheck, controller.viewDetail);
-router.get('/viewDetail/download/:id/:filename', ssc.sessionCheck, controller.download);
+router.route('/new/:title').get(ssc.sessionCheck, controller.new);
+router.route('/new').post(upload.array('incident[attach-file]'), ssc.sessionCheck, controller.save);
+router.route('/viewDetail/:id').get(ssc.sessionCheck, controller.viewDetail);
+router.route('/viewDetail/download/:id/:filename').get(ssc.sessionCheck, controller.download);
 
 module.exports = router;
