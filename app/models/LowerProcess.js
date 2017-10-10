40b26924c6e5f238f3dceb8241913938aebfccd5
sykim <ksy0226@isu.co.kr>
2017-10-10 14:02:19 +0900

    #KSY 하위코드관리

--- app/models/LowerProcess.js
+++ app/models/LowerProcess.js
@@ -3,20 +3,18 @@
 var mongoose = require('mongoose');
 
 var lowerProcessSchema = mongoose.Schema({
-    lower_cd: { type: String, required: true },     //하위업무코드
-    lower_nm: { type: String},                      //하위업무명
-    higher_cd: { type: String, required: true },     //상위업무코드
-    higher_nm: { type: String},                      //상위업무명
-    description: { type: String },                   //설명
-    need_hour: { type: String },                   //hour
-    com_cd: { type: String },                        //회사코드
-    sabun: { type: String },                         //사번
-    created_at: { type: Date, default: Date.now },    //생성일자 
-    user_flag: { type: String }                      //사용여부(1:사용, 0:미사용)
-
+    lower_cd:       { type: String, required: true },       //하위업무코드
+    lower_nm:       { type: String },                       //하위업무명
+    higher_cd:      { type: String, required: true },       //상위업무코드
+    higher_nm:      { type: String },                       //상위업무명
+    description:    { type: String },                       //설명
+    need_hour:      { type: String },                       //소요시간
+    com_cd:         { type: String },                       //회사코드
+    sabun:          { type: String },                       //사번
+    user_nm:        { type: String },                       //등록자이름
+    created_at:     { type: Date, default: Date.now },      //생성일자 
+    user_flag:      { type: String }                        //사용여부(1:사용, 0:미사용)
 });
 
-
-
-var LowerProcess = mongoose.model('lower', lowerProcessSchema);
+var LowerProcess = mongoose.model('lowerProcess', lowerProcessSchema);
 module.exports = LowerProcess;
--- app/routes/lowerProcess.js
+++ app/routes/lowerProcess.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
-
+const ssc = require('../util/session');
 const controller = require('../controllers/lowerProcess');
 
-router.route('/list').get(controller.list);
+router.route('/').get(ssc.sessionCheck, controller.index);
+router.route('/new').get(ssc.sessionCheck, controller.new)
+                    .post(ssc.sessionCheck, controller.save);
+router.route('/edit/:id').get(ssc.sessionCheck, controller.edit)
+                         .post(ssc.sessionCheck, controller.update);
+router.route('/save/:id').get(ssc.sessionCheck, controller.update);
+router.route('/delete/:id').get(ssc.sessionCheck, controller.delete);
 
 module.exports = router;
