a3126f4fb4889db40d440c83aeaf87896c46becf
lhi <hilee.em@gmail.com>
2017-10-10 18:16:48 +0900

    #incident

--- app/public/isu/js/incident/new.js
+++ app/public/isu/js/incident/new.js
@@ -25,6 +25,14 @@ $(document).ready(function () {
         todayHighlight: true,
         format: "yyyy/mm/dd"
     });
+
+    $('#saveBtn').on('click',function(){
+        if(checkValue()){
+            if(confirm("등록하시겠습니까?")){
+                $('#form').submit();
+            }
+        }
+    })
     
 });
 
@@ -35,6 +43,29 @@ function selectedHighProcess(obj){
     }else{
         $('#app_menu').slideUp(350);            
     }
+}
+
+//필수값 체크
+function checkValue(){
+    if($('#higher_cd').val() == ''){
+        alert("요청업무를 선택하세요.");
+        $('#higher_cd').focus();
+        return false;
+    }
+
+    if($('input[name="incident[real_register_mm]"]').val() == ''){
+        alert("요청자를 입력하세요.");
+        $('input[name="incident[real_register_mm]"]').focus();
+        return false;
+    }
+
+    if($('input[name="incident[title]"]').val() == ''){
+        alert("제목을 입력하세요.");
+        $('input[name="incident[title]"]').focus();
+        return false;
+    }
+
+    return true;
 }
 
 
