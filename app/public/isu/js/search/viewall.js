5cf6e12147ab817fd4291524f549b9dfa9767126
sunwoo <psw@isu.co.kr>
2017-10-11 14:41:00 +0900

    .

--- app/public/isu/js/search/viewall.js
+++ app/public/isu/js/search/viewall.js
@@ -45,13 +45,11 @@ $(document).ready(function () {
     
     //하위업무 변경 시
     $('#lower_nm').on('change', function () {
-        alert($('#lower_nm').val());
         research();
     });
 
     //TR 클릭 시
     $('#dataTR').on('click', function () {
-        alert("22222");
         window.location = $(this).attr('href');
         return false;
     });
@@ -133,11 +131,6 @@ function getDataList(){
 
 //내용 매핑
 function setDataList(dataObj) {
-    alert('dataObj.length'+dataObj.length);
-    
-    //더보기 버튼 처리
-    //setMoreBtn(dataObj);
-
     //조회 내용 추가
     if (rowIdx < dataObj.length) {
 
