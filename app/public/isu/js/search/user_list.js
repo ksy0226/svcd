835f4180aca29d8da69c166b1bdf5a9f902f4a9f
sunwoo <psw@isu.co.kr>
2017-10-13 16:32:31 +0900

    .

--- app/public/isu/js/search/user_list.js
+++ app/public/isu/js/search/user_list.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
         todayHighlight: true,
         format: "yyyy-mm-dd"
     });
+    
     $('#reg_date_to').datepicker({
         autoclose: true,
         todayHighlight: true,
@@ -24,9 +25,12 @@ $(document).ready(function () {
             research();
         }
     });
+
+    //최초 페이징
+    paging(totalData, dataPerPage, pageCount, 1);
     
     //최초 조회
-    getDataList();
+    getDataList(1);
 
     //조회버튼 클릭 시
     $('#searchBtn').on('click', function () {
@@ -44,22 +48,22 @@ $(document).ready(function () {
         research();
     });
 
-    $('#paging').on('click', function () {
-        research();
+    $('#paging').on('click', function (selectedPage) {
+        alert("page click");
+        //research(selectedPage);
     }); 
 
 });
 
 //다시 조회
-function research(){
+function research(selectedPage){
     dataCnt = 0;
     rowIdx = 0;
 
     //내용삭제
     $("#more_list").empty();
-    getDataList();
+    getDataList(selectedPage);
 }
-
 
 /**
  * 하위업무 가져오기
@@ -81,6 +85,7 @@ function getLowerProcessList(){
         }
     });
 }
+
 /**
  * 하위업무 뿌리기
  */
@@ -97,11 +102,75 @@ function setContent(data, higher_cd){
     }
 }
 
+
+
+var totalData = 322;    // 총 데이터 수
+var dataPerPage = 15;    // 한 페이지에 나타낼 데이터 수
+var pageCount = 6;       // 한 화면에 나타낼 페이지 수
+
 /**
- * 선택된 리스트 가져오기 (상/하위업무 구분, 검색어, 날짜)
+ * 페이징 처리
  */
-function getDataList(){
+function paging(totalData, dataPerPage, pageCount, currentPage){
     
+    var totalPage = Math.ceil(totalData/dataPerPage);    // 총 페이지 수
+    var pageGroup = Math.ceil(currentPage/pageCount);    // 페이지 그룹
+    //alert("currentPage : " + currentPage);
+    //alert("pageGroup : " + pageGroup);
+    
+    var last = pageGroup * pageCount;    // 화면에 보여질 마지막 페이지 번호
+    if(last > totalPage)
+        last = totalPage;
+    var first = last - (pageCount-1);    // 화면에 보여질 첫번째 페이지 번호
+    var next = last+1;
+    var prev = first-1;
+    
+    //alert("last : " + last);
+    //alert("first : " + first);
+    //alert("next : " + next);
+    //alert("prev : " + prev);
+
+    var $pingingView = $("#paging");
+    
+    var html = "";
+    
+    if(prev > 0)
+        html += "<a href=# id='prev'><<<</a> ";
+    for(var i=first; i <= last; i++){
+        html += "<a href='#' id=" + i + ">" + i + "</a> ";
+    }
+    
+    if(last < totalPage)
+        html += "<a href=# id='next'>>>></a>";
+    
+    $("#paging").html(html);    // 페이지 목록 생성
+    $("#paging a").css("color", "black");
+    $("#paging a#" + currentPage).css({"text-decoration":"none", 
+                                    "color":"red", 
+                                    "font-weight":"bold",});    // 현재 페이지 표시
+    
+    //페이지 목록 선택 시 1페이징 함수, 2데이터 조회 함수 호출
+    $("#paging a").click(function(){
+        
+        var $item = $(this);
+        var $id = $item.attr("id");
+        var selectedPage = $item.text();
+        
+        if($id == "next")    selectedPage = next;
+        if($id == "prev")    selectedPage = prev;
+        
+        paging(totalData, dataPerPage, pageCount, selectedPage);
+        //getPagingData(dataPerPage, selectedPage);
+        getDataList(selectedPage);
+    });
+                                    
+}
+
+/**
+ * 데이터 조회-(상/하위업무 구분, 검색어, 날짜 중) 선택된 리스트 가져오기 
+ */
+function getDataList(selectedPage){
+    alert("getDataList 함수 selectedPage" + selectedPage);
     if($('#lower_cd').val() =="" || $('#lower_cd').val() ==null){
         $('#lower_cd').val("*");
     }
@@ -124,22 +193,36 @@ function getDataList(){
         },
         success: function (dataObj) {
             $('#ajax_indicator').css("display", "none");
-            setDataList(dataObj);
+            //리스트에 내용 매핑
+            setDataList(dataObj, selectedPage);
         }
     });
 }
-function getPagingData(dataPerPage, selectedPage){
-    alert("선택한 페이지   :   " + selectedPage);
-    alert("페이지당 수    :   " + dataPerPage);
+
+
+/**
+ * 선택된 내용 매핑하기
+ */
+function setDataList(dataObj, selectedPage) {
+    alert("setDataList 함수 "+ selectedPage);
+    alert("setDataList 선택한 페이지   :   " + selectedPage);
+
+    //선택한 페이지가 1page 이상일 때,
+    if(selectedPage>1){
+        //기존 데이터 삭제
+        $("#more_list tr").remove();
+    }
+
+    //var totalData = dataObj.length();
+    //alert("totalData 토탈데이터 수" + totalData);
+    
+    dataPerPage = 15;   //페이지당 수
     
     var startIdx = dataPerPage*(selectedPage-1)+1; 
     var endIdx = dataPerPage*selectedPage+1; 
     
-    
     for(var i = startIdx ; i < endIdx ; i++){
-        alert("startIdx"+startIdx);
-        alert("endIdx"+endIdx);
-        /*
+        alert(i+"번째"+dataObj[i].title);
         var register_dateVal = dataObj[i].register_date; 
         register_dateVal = register_dateVal.substring(0,10);
         var idValue = dataObj[i]._id ;
@@ -153,15 +236,20 @@ function getPagingData(dataPerPage, selectedPage){
         addList += "							</tr>";
 
         $("#more_list").append(addList);
-        */
+
+        startIdx++;
+        
     }
-}
 
-/**
- * 선택된 내용 매핑하기
- */
-function setDataList(dataObj) {
 
+
+
+    /*
+    if(selectedPage >1){
+        $("#more_list").remove();
+    }
+    */
+    /*
     if (rowIdx < dataObj.length) {
 
         if ((rowIdx + inCnt) < dataObj.length) {
@@ -188,35 +276,10 @@ function setDataList(dataObj) {
 
                 rowIdx++;
             }
-
-            //dataCnt = dataCnt * 2; //
-
-            //alert("dataCnt*2 = "+dataCnt);
-
-            /*for (var i = rowIdx; i < dataCnt; i++) {
-                alert("for"+dataCnt);
-                var register_dateVal = dataObj[i].register_date; 
-                register_dateVal = register_dateVal.substring(0,10);
-                var idValue = dataObj[i]._id ;
-                var addList = "";
-                addList += "							<tr onclick=window.location='/search/user_detail/" + dataObj[i]._id + "'>";
-                addList += "								<td>" + dataObj[i].higher_nm + "</td>";
-                addList += "								<td>" + dataObj[i].lower_nm + "</td>";
-                addList += "								<td>" + dataObj[i].title + "</td>";
-                addList += "								<td>" + register_dateVal + "</td>";
-                addList += "								<td>" + dataObj[i].manager_nm + "</td>";
-                addList += "							</tr>";
-
-                $("#more_list").append(addList);
-
-                rowIdx++;
-            }
-            */
-
-        //}else{
-        //    alert("dataCnt 수가 더 크거나 같음");
-        //}
-
+    
     }
+    */
+    
+
 
 }
