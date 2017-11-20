0596328b6aea4218014d2aa61c86a3cb9efe1a79
sykim <ksy0226@isu.co.kr>
2017-11-20 14:48:02 +0900

    #KSY 메인 수정

--- app/public/isu/js/main/admin.js
+++ app/public/isu/js/main/admin.js
@@ -9,69 +9,7 @@ $(document).ready(function () {
     getDataList();
     //메인 카운트 로드
     cntLoad();
-    //차트 로드
-    chartLoad();
 });
-
-//차트 로드
-function chartLoad() {
-    $.ajax({
-        type: "GET",
-        async: true,
-        url: "/statistic/chartLoad",
-        dataType: "json", // xml, html, script, json 미지정시 자동판단
-        timeout: 30000,
-        cache: false,
-        data: {},
-        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-        error: function (request, status, error) {
-            alert("error : " + error);
-        },
-        beforeSend: function (dataObj) {
-        },
-        success: function (dataObj) {
-            setChartLoad(dataObj);
-        }
-    });
-}
-
-function setChartLoad(dataObj) {
-    var DrawSparkline = function () {
-        $('#sparkline1').sparkline([60, 23, 43, 35, 44, 45, 56, 37, 40], {
-            type: 'line',
-            width: $('#sparkline1').width(),
-            height: '165',
-            chartRangeMax: 50,
-            lineColor: '#3bafda',
-            fillColor: 'rgba(59,175,218,0.3)',
-            highlightLineColor: 'rgba(0,0,0,.1)',
-            highlightSpotColor: 'rgba(0,0,0,.2)',
-        });
-
-        $('#sparkline1').sparkline([25, 23, 26, 24, 25, 32, 30, 24, 19], {
-            type: 'line',
-            width: $('#sparkline1').width(),
-            height: '165',
-            chartRangeMax: 40,
-            lineColor: '#00b19d',
-            fillColor: 'rgba(0, 177, 157, 0.3)',
-            composite: true,
-            highlightLineColor: 'rgba(0,0,0,.1)',
-            highlightSpotColor: 'rgba(0,0,0,.2)',
-        });
-    };
-
-    DrawSparkline();
-
-    var resizeChart;
-
-    $(window).resize(function (e) {
-        clearTimeout(resizeChart);
-        resizeChart = setTimeout(function () {
-            DrawSparkline();
-        }, 300);
-    });
-}
 
 //메인 카운트 로드
 function cntLoad() {
