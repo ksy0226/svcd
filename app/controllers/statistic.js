962a243bf280b88ac11ff8bf27f7de0b40924ee7
psw <psw@isu.co.kr>
2017-11-21 09:30:03 +0900

    #psw 메인 수정중

--- app/controllers/statistic.js
+++ app/controllers/statistic.js
@@ -187,9 +187,13 @@ module.exports = {
      * 당월 처리현황 조회
     */
     monthlyload : (req, res, next) => {
-        var startDate = new Date(new Date().setDate(new Date().getDate()-60)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        var endDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        
+        //var startDate = new Date(new Date().setDate(new Date().getDate()-60)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        //var endDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        var today = new Date();
+        var thisYear = today.getFullYear();
+
+
+
         var aggregatorOpts = 
         [
             { 
@@ -204,23 +208,18 @@ module.exports = {
                     //,register_date : {$gte: "2017-10-19T04:49:38.881Z", $lte:"2017-11-15T04:49:38.881Z"}
                     //,register_date : {$gte: new Date(new Date().setDate(new Date().getDate()-180)), $lte: new Date()}
                     
-                    ,register_date : { $gte : startDate, $lte : endDate }
+                    ,register_yyyy : thisYear
 
                 }
             }
             ,{ 
                 $group : { //그룹칼럼
                     _id: {
-                        //status_cd : "$status_cd"
-                        register_yyyy : "$register_yyyy"
-                        ,register_mm : "$register_mm"
-                        //,valuation : "$valuation"
-                        //status_cd: { $ifNull: [ '$status_cd', [{ count: 0 }] ] }
+                        //register_yyyy : "$register_yyyy"
+                        register_mm : "$register_mm"
                     }
                     ,count: {
                         $sum : 1
-                        //$sum : { $ifNull: [ $sum, 0 ] }
-                        //$sum :{ $ifNull: [ "$count", 1] }
                     }
                     ,avgValue: { $avg: "$valuation" }
                     
@@ -241,7 +240,7 @@ module.exports = {
             }
         ]
 
-        console.log("monthlyload aggregatorOpts >> "+JSON.stringify(aggregatorOpts)); 
+        console.log("monthlyload aggregatorOpts >> "+JSON.stringify(aggregatorOpts)+thisYear); 
         IncidentModel.aggregate(aggregatorOpts).exec(function (err, incident) {
         //IncidentModel.count({status_cd: '4', manager_company_cd : "ISU_ST", manager_sabun : "14002"}, function (err, incident) {
             console.log("monthlyload incident >> "+JSON.stringify(incident));    
