7ca0bb71d39dbb944be13d189d8c7dec175a0aee
sykim <ksy0226@isu.co.kr>
2017-11-21 18:42:45 +0900

    #KSY 차트 수정 중

--- app/controllers/statistic.js
+++ app/controllers/statistic.js
@@ -20,18 +20,18 @@ module.exports = {
                          }
             },    
             */
-            { 
-                $group : { //그룹칼럼
-                            _id: {
-                                request_company_cd: "$request_company_cd"
-                                //higher_cd: "$higher_cd",
-                                //lower_cd: "$lower_cd"
-                            },
-                            count: {
-                                $sum: 1
-                            }
-                         }
-              }
+            {
+                $group: { //그룹칼럼
+                    _id: {
+                        request_company_cd: "$request_company_cd"
+                        //higher_cd: "$higher_cd",
+                        //lower_cd: "$lower_cd"
+                    },
+                    count: {
+                        $sum: 1
+                    }
+                }
+            }
         ]
 
         IncidentModel.aggregate(aggregatorOpts)
@@ -68,38 +68,38 @@ module.exports = {
         });
         */
         var aggregatorOpts = [
-            
-            { 
-                $match : { //조건
-                            request_company_cd : "ISU_ST"
-                            ,higher_cd : "H001"
-                            ,$or: [ { status_cd : "2" }, { status_cd : "3" }, { status_cd : "4" }]
-                            
-                         }
+
+            {
+                $match: { //조건
+                    request_company_cd: "ISU_ST"
+                    , higher_cd: "H001"
+                    , $or: [{ status_cd: "2" }, { status_cd: "3" }, { status_cd: "4" }]
+
+                }
             }
-               
-            
-            ,{ 
-                $group : { //그룹칼럼
+
+
+            , {
+                $group: { //그룹칼럼
                     _id: {
                         //request_company_cd: "$request_company_cd"
-                        higher_cd : "$higher_cd"
-                        ,higher_nm : "$higher_nm"
-                        ,lower_cd : "$lower_cd"
-                        ,lower_nm : "$lower_nm"
-                        
+                        higher_cd: "$higher_cd"
+                        , higher_nm: "$higher_nm"
+                        , lower_cd: "$lower_cd"
+                        , lower_nm: "$lower_nm"
+
                     }
-                    ,count: {
+                    , count: {
                         $sum: 1
                     }
                 }
-            } 
-              
+            }
+
         ]
         //logger.debug('aggregatorOpts'+aggregatorOpts);
         IncidentModel.aggregate(aggregatorOpts)
             .exec(function (err, incident) {
-                logger.debug("incident"+JSON.stringify(incident));
+                logger.debug("incident" + JSON.stringify(incident));
                 if (err) {
                     res.render("http/500", {
                         err: err
@@ -109,7 +109,7 @@ module.exports = {
                     //res.json(result);
                     incident: incident
                 }
-    
+
                 res.render("statistic/high_lower", {
                     incident: incident
                 });
@@ -119,114 +119,107 @@ module.exports = {
     /**
      * 메인 카운트 로드
     */
-    cntload : (req, res, next) => {
-        var startDate = new Date(new Date().setDate(new Date().getDate()-60)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    cntload: (req, res, next) => {
+        var startDate = new Date(new Date().setDate(new Date().getDate() - 60)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
         var endDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        
-        var aggregatorOpts = 
-        [
-            { 
-                $match : { //조건
-                    manager_company_cd : "ISU_ST"  //각 사별 관리담당자는?
-                    //,manager_sabun : "12001"     //req.session.sabun 넣을 예정
-                    ,$or: [ { status_cd : "1" }, { status_cd : "2" }, { status_cd : "3" }, { status_cd : "4" }]
-                    /*,$and : [ { register_date : {$gte: "2017-05-19T04:49:38.881Z"}}
-                             ,{ register_date : {$lte:"2017-11-15T04:49:38.881Z"}} ]
-                    */    
-                    //,register_date : {$gte: "2017-10-19T04:49:38.881Z", $lte:"2017-11-15T04:49:38.881Z"}
-                    //,register_date : {$gte: new Date(new Date().setDate(new Date().getDate()-180)), $lte: new Date()}
-                    
-                    ,register_date : { $gte : startDate, $lte : endDate }
 
-                }
-            }
-            ,{ 
-                $group : { //그룹칼럼
-                    _id: {
-                        status_cd : "$status_cd"
-                        //status_cd: { $ifNull: [ '$status_cd', [{ count: 0 }] ] }
+        var aggregatorOpts =
+            [
+                {
+                    $match: { //조건
+                        manager_company_cd: "ISU_ST"  //각 사별 관리담당자는?
+                        //,manager_sabun : "12001"     //req.session.sabun 넣을 예정
+                        , $or: [{ status_cd: "1" }, { status_cd: "2" }, { status_cd: "3" }, { status_cd: "4" }]
+                        /*,$and : [ { register_date : {$gte: "2017-05-19T04:49:38.881Z"}}
+                                 ,{ register_date : {$lte:"2017-11-15T04:49:38.881Z"}} ]
+                        */
+                        //,register_date : {$gte: "2017-10-19T04:49:38.881Z", $lte:"2017-11-15T04:49:38.881Z"}
+                        //,register_date : {$gte: new Date(new Date().setDate(new Date().getDate()-180)), $lte: new Date()}
+
+                        , register_date: { $gte: startDate, $lte: endDate }
+
                     }
-                    ,count: {
-                        $sum : 1
-                        //$sum : { $ifNull: [ $sum, 0 ] }
-                        //$sum :{ $ifNull: [ "$count", 1] }
+                }
+                , {
+                    $group: { //그룹칼럼
+                        _id: {
+                            status_cd: "$status_cd"
+                            //status_cd: { $ifNull: [ '$status_cd', [{ count: 0 }] ] }
+                        }
+                        , count: {
+                            $sum: 1
+                            //$sum : { $ifNull: [ $sum, 0 ] }
+                            //$sum :{ $ifNull: [ "$count", 1] }
+                        }
+
                     }
-                    
                 }
-            }
-            /*
-            , {
-                total: { 
-                    $sum: "$count"
-                } 
-            }
-            */
-            , {
-                $sort : {
-                    status_cd: -1
+                /*
+                , {
+                    total: { 
+                        $sum: "$count"
+                    } 
                 }
-            }
-        ]
-        
+                */
+                , {
+                    $sort: {
+                        status_cd: -1
+                    }
+                }
+            ]
+
         IncidentModel.aggregate(aggregatorOpts).exec(function (err, incident) {
-        //IncidentModel.count({status_cd: '4', manager_company_cd : "ISU_ST", manager_sabun : "14002"}, function (err, incident) {
-        //console.log("incident"+JSON.stringify(incident));    
-        if (err) {
+            //IncidentModel.count({status_cd: '4', manager_company_cd : "ISU_ST", manager_sabun : "14002"}, function (err, incident) {
+            //console.log("incident"+JSON.stringify(incident));    
+            if (err) {
                 return res.json({
                     success: false,
                     message: err
                 });
-            } 
+            }
 
             res.json(incident);
         });
     },
 
-    chartLoad : (req, res, next) => {
+    chartLoad: (req, res, next) => {
         var today = new Date();
         var thisYear = today.getFullYear();
-        var preYear = thisYear-1;
+        var preYear = thisYear - 1;
 
-        console.log("thisYear   >>>>>>>",thisYear);
-        console.log("preYear    >>>>>>>",preYear);
-        //var startDate = new Date(new Date().setDate(new Date().getDate()-60)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        //var endDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        var aggregatorOpts =
+            [
+                {
+                    $match: { //조건
+                        register_yyyy: { $gte: preYear.toString(), $lte: thisYear.toString() }
+                    }
+                }, {
+                    $group: { //그룹
+                        _id: {
+                            register_yyyy: "$register_yyyy",
+                            register_mm: "$register_mm"
+                        }
+                        , count: {
+                            $sum: 1
+                        }
 
-        var aggregatorOpts = 
-        [
-            { 
-                $match : { //조건
-                    register_yyyy : "2017"
-                    //register_yyyy : { $gte : preYear, $lte : thisYear }
-                    //register_date : { $gte : startDate, $lte : endDate }
-                }
-            },{ 
-                $group : { //그룹칼럼
-                    _id: {
-                        //register_yyyy : "$register_yyyy",
-                        register_mm : "$register_mm"
                     }
-                    ,count: {
-                        $sum : 1
+                }, {
+                    $sort: {
+                        register_yyyy: -1,
+                        register_mm: -1
                     }
-                    
                 }
-            },{
-                $sort : {
-                    //register_yyyy : -1,
-                    register_mm : -1
-                }
-            }
-        ]
-        
+            ]
+
         IncidentModel.aggregate(aggregatorOpts).exec(function (err, incident) {
-        console.log("chartLoad >>>>>> "+JSON.stringify(incident));    
-        if (err) {
+            console.log("chartLoad >>>>>> " + JSON.stringify(incident));
+            if (err) {
                 return res.json({
                     success: false,
                     message: err
                 });
-            } 
+            }
 
             res.json(incident);
         });
@@ -236,51 +229,51 @@ module.exports = {
     /**
      * 당월 처리현황 조회
     */
-    monthlyload : (req, res, next) => {
+    monthlyload: (req, res, next) => {
         var today = new Date();
         var thisYear = today.getFullYear();
 
-        var aggregatorOpts = 
-        [
-            { 
-                $match : { //조건
-                    //manager_company_cd : "ISU_ST"  //각 사별 관리담당자는?
-                    //,manager_sabun : "12001"     //req.session.sabun 넣을 예정
-                    status_cd : "4"
-                    ,register_yyyy : "2017"
-                }
-            }
-            ,{ 
-                $group : { //그룹칼럼
-                    _id: {
-                        //register_yyyy : "$register_yyyy"
-                        register_mm : "$register_mm"
+        var aggregatorOpts =
+            [
+                {
+                    $match: { //조건
+                        //manager_company_cd : "ISU_ST"  //각 사별 관리담당자는?
+                        //,manager_sabun : "12001"     //req.session.sabun 넣을 예정
+                        status_cd: "4"
+                        , register_yyyy: "2017"
                     }
-                    ,count: {
-                        $sum : 1
+                }
+                , {
+                    $group: { //그룹칼럼
+                        _id: {
+                            //register_yyyy : "$register_yyyy"
+                            register_mm: "$register_mm"
+                        }
+                        , count: {
+                            $sum: 1
+                        }
+                        , avgValue: { $avg: "$valuation" }
+                        //ROUND(avg(downloads),2)
+
                     }
-                    ,avgValue: { $avg: "$valuation" }
-                    //ROUND(avg(downloads),2)
-                    
                 }
-            }
-            , {
-                $sort : {
-                    register_mm : -1 
+                , {
+                    $sort: {
+                        register_mm: -1
+                    }
                 }
-            }
-        ]
+            ]
 
         //console.log("monthlyload aggregatorOpts >> "+JSON.stringify(aggregatorOpts)+thisYear); 
         IncidentModel.aggregate(aggregatorOpts).exec(function (err, incident) {
-        //IncidentModel.count({status_cd: '4', manager_company_cd : "ISU_ST", manager_sabun : "14002"}, function (err, incident) {
-        //console.log("monthlyload incident >> "+JSON.stringify(incident));    
-        if (err) {
+            //IncidentModel.count({status_cd: '4', manager_company_cd : "ISU_ST", manager_sabun : "14002"}, function (err, incident) {
+            //console.log("monthlyload incident >> "+JSON.stringify(incident));    
+            if (err) {
                 return res.json({
                     success: false,
                     message: err
                 });
-            } 
+            }
 
             res.json(incident);
         });
