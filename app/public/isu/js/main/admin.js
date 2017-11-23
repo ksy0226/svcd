eee2f46a428a398f0bd0e6668118ee6336ef2c93
psw <psw@isu.co.kr>
2017-11-23 09:27:19 +0900

    #psw

--- app/public/isu/js/main/admin.js
+++ app/public/isu/js/main/admin.js
@@ -126,7 +126,7 @@ function setMonthlyLoad(dataObj){
             barWidth: '12',
             barSpacing: '10', //3
             barColor: '#3bafda',
-            chartRangeMax: 5,     //평점 5점만점
+            chartRangeMax: 8,     //평점 5점만점
             //tooltipFormatFieldlist: ['x','y'],
             ///xaxis: {ticks: [[1,'One'], [2,'Two'], [3,'Three'], [4,'Four'], [5,'Five']]},
             
