9bbcd65a2b2c5dbefd9ad1d62db01d7d3672a9ed
sykim <ksy0226@isu.co.kr>
2017-11-23 14:17:56 +0900

    #KSY 증감표 수정

--- app/public/isu/js/statistic/chart_main.js
+++ app/public/isu/js/statistic/chart_main.js
@@ -26,7 +26,7 @@ function chartLoad() {
 }
 
 function setChartLoad(dataObj) {
-    alert(JSON.stringify(dataObj));
+    //alert(JSON.stringify(dataObj));
     //alert(JSON.stringify(dataObj[0].cnt));
     //alert(JSON.stringify(dataObj[1].cntCom));
     //alert(JSON.stringify(dataObj[2]));
@@ -47,6 +47,23 @@ function setChartLoad(dataObj) {
             fillColor: 'rgba(59,175,218,0.3)',
             highlightLineColor: 'rgba(0,0,0,.1)',
             highlightSpotColor: 'rgba(0,0,0,.2)',
+            tooltipFormat: '{{offset:offset}} {{y:value}}건',
+            tooltipValueLookups: {
+                'offset': {
+                    0: dataObj[3]._id.register_yyyy + '년' + ' 1월 :',
+                    1: dataObj[3]._id.register_yyyy + '년' + ' 2월 :',
+                    2: dataObj[3]._id.register_yyyy + '년' + ' 3월 :',
+                    3: dataObj[3]._id.register_yyyy + '년' + ' 4월 :',
+                    4: dataObj[3]._id.register_yyyy + '년' + ' 5월 :',
+                    5: dataObj[3]._id.register_yyyy + '년' + ' 6월 :',
+                    6: dataObj[3]._id.register_yyyy + '년' + ' 7월 :',
+                    7: dataObj[3]._id.register_yyyy + '년' + ' 8월 :',
+                    8: dataObj[3]._id.register_yyyy + '년' + ' 9월 :',
+                    9: dataObj[3]._id.register_yyyy + '년' + ' 10월 :',
+                    10: dataObj[3]._id.register_yyyy + '년' + ' 11월 :',
+                    11: dataObj[3]._id.register_yyyy + '년' + ' 12월 :'
+                }
+            },
         });
 
         $('#sparkline1').sparkline(dataObj[0].cnt2, {
@@ -59,6 +76,23 @@ function setChartLoad(dataObj) {
             composite: true,
             highlightLineColor: 'rgba(0,0,0,.1)',
             highlightSpotColor: 'rgba(0,0,0,.2)',
+            tooltipFormat: '{{offset:offset}} {{y:value}}건',
+            tooltipValueLookups: {
+                'offset': {
+                    0: dataObj[2]._id.register_yyyy + '년' + ' 1월 :',
+                    1: dataObj[2]._id.register_yyyy + '년' + ' 2월 :',
+                    2: dataObj[2]._id.register_yyyy + '년' + ' 3월 :',
+                    3: dataObj[2]._id.register_yyyy + '년' + ' 4월 :',
+                    4: dataObj[2]._id.register_yyyy + '년' + ' 5월 :',
+                    5: dataObj[2]._id.register_yyyy + '년' + ' 6월 :',
+                    6: dataObj[2]._id.register_yyyy + '년' + ' 7월 :',
+                    7: dataObj[2]._id.register_yyyy + '년' + ' 8월 :',
+                    8: dataObj[2]._id.register_yyyy + '년' + ' 9월 :',
+                    9: dataObj[2]._id.register_yyyy + '년' + ' 10월 :',
+                    10: dataObj[2]._id.register_yyyy + '년' + ' 11월 :',
+                    11: dataObj[2]._id.register_yyyy + '년' + ' 12월 :'
+                }
+            },
         });
     };
 
