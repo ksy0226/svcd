bc33596e066a7012b45d0e6c49245aa992f6fc8b
lhi <hilee.em@gmail.com>
2018-01-16 16:33:08 +0900

    #LHI ProcessGubun 수정

--- app/public/isu/js/processGubun/edit.js
+++ app/public/isu/js/processGubun/edit.js
@@ -2,4 +2,6 @@
 
 $(document).ready(function() { 
     $('#use_yn').val(processGubunObj.use_yn);
+    $('#higher_cd').val(processGubunObj.higher_cd);
+    $('#question_type').val(processGubunObj.question_type);
 });
